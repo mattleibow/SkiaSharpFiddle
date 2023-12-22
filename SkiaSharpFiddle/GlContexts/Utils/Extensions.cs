a0179f8a7fe3d8c7c776b8ab48895027286c941a
BandarB7 <Bandar007@gmail.com>
2023-12-22 19:25:51 +0100

    Add offscreen classes from SkiaSharp.Tests

---
+++ SkiaSharpFiddle/GlContexts/Utils/Extensions.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace HarfBuzzSharp.Tests
+{
+	public static class Extensions
+	{
+	}
+}
---
+++ SkiaSharpFiddle/GlContexts/Utils/Win32/RECT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace SkiaSharpFiddle.GlContexts
+{
+	[StructLayout(LayoutKind.Sequential)]
+	public struct RECT
+	{
+		public int left;
+		public int top;
+		public int right;
+		public int bottom;
+	}
+}
